3f334d9f1bd38d8738704b31be7e5086d7b774ce
AbdulRafaySalsoft <AbdulRafaySalsoft@bitbucket.org>
2021-11-18 04:39:03 +0500

    routing changes . scroll fixes

--- src/app/services/http-config.service.ts
+++ src/app/services/http-config.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpConfigService {
 
-  basepath = 'http://localhost:3000/employees';
+  basepath = 'https://swapi.dev/api/people/';
 
   constructor(private http: HttpClient) { }
 
   getListItems(params) {
     console.log(this.basepath + params);
-    return this.http.get(this.basepath + params);
+    return this.http.get(this.basepath);
   }
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpConfigService } from '../services/http-config.service';
 
 @Component({
   selector: 'app-tabs',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsPage implements OnInit {
 
-  constructor() { }
+  items = [];
+  items1 = {};
+  numTimesLeft = 5;
+
+  constructor(private router: Router, private httpConfigService: HttpConfigService) {
+    this.addMoreItems()
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      this.addMoreItems();
+      this.numTimesLeft -= 1;
+      event.target.complete();
+    }, 500);
+  }
+  addMoreItems() {
+    for (let i = 0; i < 15; i++) {
+      this.items.push(i);
+    }
+  }
+
+  notifications() {
+    this.router.navigate(['/notifications']);
+  }
+
+  detail() {
+    this.router.navigate(['/tabs/home/detail']);
+  }
+
+  pisca() {
+    this.httpConfigService.getListItems(this.items).subscribe(response => {
+      console.log(response);
+      this.items1 = response
+    }, error => { console.log(error) });
+    console.log(this.items1)
+  }
 
   ngOnInit() {
+    this.pisca()
   }
 
 }
