e675780a17cec6f493c80a967cea99acef6b60e3
AbdulRafaySalsoft <AbdulRafaySalsoft@bitbucket.org>
2021-11-30 01:56:01 +0500

    chat half

--- src/app/tabs/chat/chat.page.ts
+++ src/app/tabs/chat/chat.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GeneralService } from 'src/app/services/general.service';
+import { HttpConfigService } from 'src/app/services/http-config.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,17 +9,59 @@ import { Router } from '@angular/router';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
-
-  constructor(private router: Router) { }
+  chatCreation: any = [];
+  users: any = [];
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  userto_id: any;
+  constructor(public service: HttpConfigService, public generalService: GeneralService, private route: ActivatedRoute,
+    private router: Router,) { }
 
   messages() {
     this.router.navigate(['/tabs/chat/messages']);
   }
-  // notifications() {
-  //   this.router.navigate(['/notifications']);
-  // }
+
+  async getVideos(param, index) {
+    // eslint-disable-next-line no-underscore-dangle
+    this.userto_id = param._id;
+    const url = 'chats/create';
+    this.generalService.showLoader();
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const data1: any = await this.service.postApi(url, { userto_id: this.userto_id });
+    if (data1.status && data1.data) {
+      this.chatCreation = data1.data;
+      this.router.navigate(['/tabs/chat/messages']);
+      // this.service.setVideo(data1.data);
+      this.generalService.stopLoader();
+    }
+    else {
+      if (data1.status === false) {
+        this.generalService.generalErrorMessage('No Record Found');
+      }
+      this.generalService.generalErrorMessage(data1.msg);
+      console.log(data1.msg);
+    }
+  }
+
+  async getUsers() {
+    const url = 'users';
+    this.generalService.showLoader();
+    const data1: any = await this.service.getApi(url, {});
+    if (data1.status && data1.data) {
+      this.users = data1.data;
+      // this.service.setVideo(data1.data);
+      this.generalService.stopLoader();
+    }
+    else {
+      if (data1.status === false) {
+        this.generalService.generalErrorMessage('No Record Found');
+      }
+      this.generalService.generalErrorMessage(data1.msg);
+      console.log(data1.msg);
+    }
+  }
 
   ngOnInit() {
+    this.getUsers();
   }
 
 }
