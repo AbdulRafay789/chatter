cb585a3711a00ff4a3a68342a7f166587ec91141
Muhammad Haris <mharisferoz@gmail.com>
2021-12-04 16:33:43 +0500

    new commit

--- src/app/search-results/search-results.page.ts
+++ src/app/search-results/search-results.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { GeneralService } from '../services/general.service';
+import { HttpConfigService } from '../services/http-config.service';
 
 @Component({
   selector: 'app-search-results',
@@ -7,14 +9,98 @@ import { Router } from '@angular/router';
   styleUrls: ['./search-results.page.scss'],
 })
 export class SearchResultsPage implements OnInit {
-
-  constructor(private router: Router) { }
+  galleryType = "videos";
+  videosUsers = {
+    "users":[],
+    "videos":[]
+  };
+  search ="";
+  videos = [];
+  constructor(private router: Router, public service: HttpConfigService, public generalService: GeneralService) { }
 
   notifications() {
     this.router.navigate(['/notifications']);
   }
+  setVideos(elem){
+    this.videos = [];
+    elem.forEach(element => {
+      let obj = {};
+      element.videos.forEach(vid => {
+        if(vid.path){
+          obj = vid;
+          obj["_id"] = element["_id"];
+          if(vid.path.indexOf('.mp4') == -1){
+            obj['isvideo'] = false;
+          }
+          else{
+            obj['isvideo'] = true;;
+          }
+        }
+      });
+      this.videos.push(obj);
+    });
+  }
+  async searchall() {
+    this.generalService.showLoader();
+    const data1: any = await this.service.postApi('searchuserandvideos', {"searchvalue":this.search});
+    if (data1.status && data1.data) {
+      this.videosUsers = data1.data;
+      this.setVideos(this.videosUsers.videos);
+    }
+    else {
+      if (data1.status === false) {
+        this.generalService.generalErrorMessage('No Record Found');
+      }
+      this.generalService.generalErrorMessage(data1.msg);
+      console.log(data1.msg);
+    }
+    this.generalService.stopLoader();
+    // this.email = data1.email;
+    // this.password = data1.password;
+  }
+  UpdateSegment(segment){
+    this.galleryType = segment;
+  }
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
 
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.videosUsers.videos.length == 1000) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+  async gotoUser(param) {
+    this.generalService.showLoader();
+    const url = 'users/details/' + param['_id'];
+    const data1: any = await this.service.getApi(url, {});
+    if (data1.status && data1.data) {
+      this.router.navigate([
+        '/profileforusers',
+        { data: JSON.stringify(data1.data[0]) },
+      ]);
+    } else {
+      this.generalService.generalErrorMessage(data1.msg);
+      console.log(data1.msg);
+    }
+    this.generalService.stopLoader();
+  }
+  async viewVideo(param) {
+    let obj = {};
+    let indx = 0 ;
+    this.videosUsers.videos.forEach((element,i) => {
+      if(param["_id"].toString() == element["_id"].toString()){
+        obj = element;
+        indx = i;
+      }
+    });
+    this.router.navigate(['/tabs/home/detail', { data: JSON.stringify(obj), index: indx }]);
+  }
   ngOnInit() {
+    this.searchall();
   }
 
 }
